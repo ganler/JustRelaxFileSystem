c9069256687751d036e8e9fa405974a607e54684
ganler <jaway.liu@gmail.com>
2020-06-19 20:34:36 +0800

    Doc v3

--- docs/search/functions_d.js
+++ docs/search/functions_d.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['white_168',['WHITE',['../namespacept.html#a152f42a2014a8f86c778754c244d95a3',1,'pt']]],
-  ['write_169',['write',['../structjrfs_1_1data__block.html#a184373d6b0c43205cb001abb0835e057',1,'jrfs::data_block::write()'],['../structjrfs_1_1inode.html#a8b8725f0ee29a296f20aa9e5b3e89ebc',1,'jrfs::inode::write()'],['../structjrfs_1_1super__block.html#a8ac58c10f45dbefd7da687563ed000a0',1,'jrfs::super_block::write()'],['../structjrfs_1_1filesystem_1_1filehander.html#ae5ba31896370b238e3c75eab8f1243b7',1,'jrfs::filesystem::filehander::write()']]]
+  ['write_169',['write',['../structjrfs_1_1data__block.html#a184373d6b0c43205cb001abb0835e057',1,'jrfs::data_block::write()'],['../structjrfs_1_1inode.html#a8b8725f0ee29a296f20aa9e5b3e89ebc',1,'jrfs::inode::write()'],['../structjrfs_1_1super__block.html#a3c3af0de1687838a3823f0137c805489',1,'jrfs::super_block::write()'],['../structjrfs_1_1filesystem_1_1filehander.html#ae5ba31896370b238e3c75eab8f1243b7',1,'jrfs::filesystem::filehander::write()']]]
 ];
